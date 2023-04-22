c1afa8f42a8aa2259596c2e8652897fc187c787c
angelique_rosin <angie1979.capucine@yahoo.fr>
2023-04-22 23:36:29 +0200

    responsive

--- src/components/FeatureComponents/HeroSection.jsx
+++ src/components/FeatureComponents/HeroSection.jsx
@@ -7,43 +7,83 @@ const Hero = styled.div`
   background-size: cover;
   background-repeat: no-repeat;
   position: relative;
+  height: 400px;
   @media (min-width: 520px) {
     height: 400px;
-    background-position: 0% 33%;
+  }
+  @media (min-width: 845px) {
+    height: 530px;
+  }
+  @media (min-width: 1079px) {
+    height: 664px;
+  }
+  @media (min-width: 1500px) {
+    height: 864px;
+  }
+  @media (min-width: 1500px) {
+    height: 960px;
   }
 `;
 
 const HeroContent = styled.section`
   position: relative;
   top: 2rem;
-  width: 200px;
+  width: 75px;
   background: white;
-  padding: 2rem;
+  padding: 5px;
   text-align: left;
-  margin: 0 auto;
-  @media (min-width: 520px) {
+  right: -280px;
+  @media (min-width: 440px) {
+    right: -310px;
+  }
+  @media (min-width: 519px) {
+    width: 167px;
+  }
+  @media (min-width: 720px) {
     position: absolute;
     top: 50px;
     right: 50px;
     width: 300px;
     margin: 2rem;
+    padding: 10px;
+  }
+  @media (min-width: 1400px) {
+    padding: 40px;
   }
 `;
 
 const Subtitle = styled.p`
   font-weight: bold;
-  font-size: 1rem;
+  font-size: 0.6rem;
   margin: 0;
   @media (min-width: 520px) {
+    font-size: 1rem;
+  }
+
+  @media (min-width: 720px) {
+    font-size: 1.3rem;
+  }
+  @media (min-width: 1090px) {
     font-size: 1.5rem;
+  }
+
+  @media (min-width: 1400px) {
+    font-size: 1.7rem;
   }
 `;
 
 const Text = styled.p`
   margin-bottom: 0;
-  font-size: 0.9rem;
-  @media (min-width: 520px) {
-    font-size: 1.2rem;
+  font-size: 0.5rem;
+  @media (min-width: 720px) {
+    font-size: 1.1rem;
+  }
+  @media (min-width: 1090px) {
+    font-size: 1.3rem;
+  }
+
+  @media (min-width: 1400px) {
+    font-size: 1.5rem;
   }
 `;
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Navbar from "../components/NavBar";
+import styled from "styled-components";
 import Footer from "../components/Footer";
 import FeatureItems from "../components/FeatureComponents/FeatureItems";
 import HeroSection from "../components/FeatureComponents/HeroSection";
+
+// const MainContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   min-height: 100vh;
+// `;
 
 const LandingPage = () => {
   return (
